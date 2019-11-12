1da18227bf9abb38ad6569bd1c26c4d9bd22fd16
sslavik <sslavikshy@gmail.com>
2019-11-12 13:05:33 +0100

    First Commit with Unity

---
+++ Pruebas 3/Assets/Scripts/bg_change.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bg_change : MonoBehaviour {
+
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float r = 0;
+		float g = 0;
+		float b = 0;
+		
+		r = 220 + Mathf.PingPong(Time.time,35);
+		g = 220 + Mathf.PingPong(Time.time,35);
+		b = 220 + Mathf.PingPong(Time.time,35);
+		cam.backgroundColor = new Color(r,g,b);
+
+	}
+}
---
+++ Pruebas 3/Assets/Scripts/menu.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour {
+	// CAMPOS
+	public Button btnMenu;
+	GameObject tgSonido;
+	GameObject tgLuz;
+	GameObject dlLuz;
+	GameObject mainCamera;
+	GameObject sliderVolume;
+	GameObject ddWeek;
+
+	// Use this for initialization
+	void Start () {
+		// INSTANCIAMOS 
+		// LUZ & SONIDO
+		tgLuz = GameObject.Find("tgLuz");
+		tgSonido = GameObject.Find("tgSonido");
+		dlLuz = GameObject.Find("Directional Light");
+		// CAMARA
+		mainCamera = GameObject.Find("Main Camera");
+		// VOLUMEN
+		sliderVolume = GameObject.Find("slVolumen");
+		// DROPDOWN
+		ddWeek = GameObject.Find("ddWeek");
+		ddWeek.GetComponent<Dropdown>().options.Clear();
+		ddWeek.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData("Lunes"));
+		ddWeek.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData("Martes"));
+		ddWeek.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData("Miércoles"));
+		ddWeek.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData("Jueves"));
+		ddWeek.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData("Viernes"));
+		ddWeek.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData("Sábado"));
+		ddWeek.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData("Domingo"));
+		ddWeek.GetComponent<Dropdown>().value = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape))
+			Salir();
+		// USO DE TOGGLE
+		dlLuz.SetActive(tgLuz.GetComponent<Toggle>().isOn);
+		if(tgSonido.GetComponent<Toggle>().isOn)
+			mainCamera.GetComponent<Camera>().GetComponent<AudioSource>().UnPause();
+		else 
+		// CON 
+			mainCamera.GetComponent<Camera>().GetComponent<AudioSource>().Pause();
+
+		// USO DE SLIDER
+		mainCamera.GetComponent<Camera>().GetComponent<AudioSource>().volume = sliderVolume.GetComponent<Slider>().value;	
+	}
+
+
+	// MENU
+	// SALIR DEL PROGRAMA CON Application.Quit()
+	public void Salir(){
+		Application.Quit();
+	}
+	public void ShowMenu(){
+		gameObject.SetActive((gameObject.active?false:true));
+	}
+	// CAMBIO DE ESCENA CON SceneManager.LoadScene
+	public void ChangeScene1(){
+		SceneManager.LoadScene("Escena_1");
+	}
+
+	public void ChangeScene2(){
+		SceneManager.LoadScene("Escena_2");
+	}
+	
+}
